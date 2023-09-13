2f7b89116e9c0f12684c165fd4a3b5c75e70dd25
majusantos <124818845+majusantos@users.noreply.github.com>
2023-09-13 17:41:52 -0300

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -11,4 +11,44 @@ const UsuariosController = require  ('../controllers/usuarios');
 const VagasController = require  ('../controllers/vagas');
 
 // definição das rotas
-router.get('/candidato_vaga, CandidatoVagaContoller.listar')
+router.get('/candidato_vaga', CandidatoVagaController.listarCandidatoVaga);
+// post
+// patch
+// delete
+
+router.get('/candidatos', CandidatosController.listarCandidatos);
+// post
+// patch
+// delete
+
+router.get('/cursos', CursosController.listarCursos);
+// post
+// patch
+// delete
+
+router.get('/empresas', EmpresasControllerController.listarEmpresas);
+// post
+// patch
+// delete
+
+router.get('/experiencias', ExperienciassController.listarExperiencias);
+// post
+// patch
+// delete
+
+router.get('/usuarios', UsuariosController.listarUsuarios);
+// post
+// patch
+// delete
+
+router.get('/vagas', VagasController.listarVagas);
+// post
+// patch
+// delete
+
+module.children.exports = router;
+
+
+
+
+
